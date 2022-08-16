486dee92820441db74eeaf51d68433c3b55c5c84
Kmiatelli <kmiatelli@gmail.com>
2022-08-16 00:23:43 -0300

    Atualizado

--- Ex_07_EstruturaRepeticao.py
+++ Ex_07_EstruturaRepeticao.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 Faça um programa que leia 5 números e informe o maior número.
 """
@@ -14,20 +13,5 @@ for i in range(0, 5):
 
 
 
-=======
-"""
-Faça um programa que leia 5 números e informe o maior número.
-"""
 
 
-num = []
-
-for i in range(0, 5):
-    n = int(input(f'Digite um número: '))
-    num.append(n)
-    print(max(num))
-
-
-
-
->>>>>>> 4d7b8d251d0a648873e836b98b3900afcfa0e184
